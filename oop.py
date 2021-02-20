96956bc89506e21c9fbd0c25df75dac21adfb25e
Joseph Henriquez <joseph.henriquez@gmail.com>
2021-02-20 00:33:16 -0500

    just the basics, nothing to see here

---
+++ oop.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self, ID, salary):
+        self.ID = ID
+        self.__salary = salary  # salary is a private property
+
+    # private method
+    def __displayID(self):
+        print(f"ID: {self.ID}")
+
+me = Employee(3789, 2500)
+print("ID:", me.ID)
