16cdcc17870ada43dc425a3ca06d5eb039ce9b32
Joseph Henriquez <joseph.henriquez@gmail.com>
2021-02-21 00:21:24 -0500

    learning about the collections module

---
+++ collections_demo.py
@@ -0,0 +1,35 @@
+from collections import ChainMap, Counter, defaultdict, deque
+
+# chainmap example
+car_parts = {'hood': 500, 'engine': 5000, 'front_door': 750}
+car_options = {'A/C': 1000, 'Turbo': 2500, 'rollbar': 300}
+car_accessories = {'cover': 100, 'hood_ornament': 150, 'seat_cover': 99}
+
+# combine all the maps together
+car_pricing = ChainMap(car_accessories, car_options, car_parts)
+print (car_pricing['hood']) #500
+
+# counter example
+print (Counter('superfluous'))
+#Counter({'u': 3, 's': 2, 'e': 1, 'l': 1, 'f': 1, 'o': 1, 'r': 1, 'p': 1})
+
+counter = Counter('superfluous')
+print (counter['u']) #3
+
+# defaultdict example
+# you can set a default value if the value is not present
+sentence = "The red for jumped over the fence and ran to the zoo for food"
+words = sentence.split(' ')
+
+# you can also pass a lambda to this constructor
+d = defaultdict(int)
+for word in words:
+    d[word] += 1
+
+print(d)
+
+# deque example
+numbers = deque([1,2,3,4,5])
+numbers.append(6)
+numbers.appendleft(0)
+print(numbers)
